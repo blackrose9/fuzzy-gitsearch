c862566c31630d9462cf2b54f57018a0973d53bd
blackrose9 <sarah.mwendwa@gmail.com>
2020-01-19 20:46:20 +0300

    Add user and repository folders, components and html Edited app.component.html to custom html page

---
+++ src/app/repository/repository-component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+   selector: "app-repository",
+   templateUrl: "./repository-component.html"
+})
+
+export class Repository {
+
+   constructor(){}
+}
