f71d216dfd3892db6eb4299f2895509fd8b5d75e
blackrose9 <sarah.mwendwa@gmail.com>
2020-01-19 20:48:09 +0300

    Declare new classes in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { User } from './user/user.component';
+import { Repository } from './repository/repository-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    User,
+    Repository
   ],
   imports: [
     BrowserModule,
